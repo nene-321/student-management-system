cea36dd26509275960bfa2d2327b6a1d64a34da4
EJ Agtunong <ejohnityy32@gmail.com>
2024-01-28 18:27:35 +0800

    changes to SystemMenu window

--- student-management-system/Program.cs
+++ student-management-system/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using STUDENT_MANAGEMENT_SYSTEM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace student_management_system
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SystemMenu());
         }
     }
 }
