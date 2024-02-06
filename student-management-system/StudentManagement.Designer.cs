45a238a1edb0754b73605d3f5b655d1122d0a24d
EJ Agtunong <ejohnityy32@gmail.com>
2024-02-06 13:23:14 +0800

    change design

--- student-management-system/StudentManagement.Designer.cs
+++ student-management-system/StudentManagement.Designer.cs
@@ -52,9 +52,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Copperplate Gothic Bold", 20F);
-            this.label2.Location = new System.Drawing.Point(310, 285);
+            this.label2.Location = new System.Drawing.Point(465, 438);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(593, 30);
+            this.label2.Size = new System.Drawing.Size(886, 44);
             this.label2.TabIndex = 1;
             this.label2.Text = "Select student information to access";
             // 
@@ -63,18 +64,18 @@
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(81)))), ((int)(((byte)(15)))));
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(167, 0);
+            this.panel1.Location = new System.Drawing.Point(250, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(838, 88);
+            this.panel1.Size = new System.Drawing.Size(1257, 135);
             this.panel1.TabIndex = 3;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
-            this.panel3.Location = new System.Drawing.Point(-168, 70);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Location = new System.Drawing.Point(-252, 108);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1006, 18);
+            this.panel3.Size = new System.Drawing.Size(1509, 28);
             this.panel3.TabIndex = 19;
             // 
             // label1
@@ -82,11 +83,12 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 19F);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
-            this.label1.Location = new System.Drawing.Point(7, 29);
+            this.label1.Location = new System.Drawing.Point(10, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(330, 29);
+            this.label1.Size = new System.Drawing.Size(478, 43);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Student Grades Data";
+            this.label1.Text = "Student Management";
             // 
             // tableLayoutPanel1
             // 
@@ -99,25 +101,27 @@
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 5);
             this.tableLayoutPanel1.Controls.Add(this.button3, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(-1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-2, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 215F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.99848F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.00051F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.00051F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.00051F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 56F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(169, 553);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 86F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(254, 851);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
             this.button1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 15F);
-            this.button1.Location = new System.Drawing.Point(3, 143);
+            this.button1.Location = new System.Drawing.Point(4, 220);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 83);
+            this.button1.Size = new System.Drawing.Size(244, 127);
             this.button1.TabIndex = 1;
             this.button1.Text = "Student grades data";
             this.button1.UseVisualStyleBackColor = false;
@@ -126,9 +130,10 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
             this.button2.Font = new System.Drawing.Font("Copperplate Gothic Bold", 15F);
-            this.button2.Location = new System.Drawing.Point(3, 232);
+            this.button2.Location = new System.Drawing.Point(4, 357);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(163, 83);
+            this.button2.Size = new System.Drawing.Size(244, 127);
             this.button2.TabIndex = 2;
             this.button2.Text = "Student attendance data";
             this.button2.UseVisualStyleBackColor = false;
@@ -137,9 +142,10 @@
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
             this.button4.Font = new System.Drawing.Font("Copperplate Gothic Bold", 15F);
-            this.button4.Location = new System.Drawing.Point(3, 410);
+            this.button4.Location = new System.Drawing.Point(4, 631);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(163, 83);
+            this.button4.Size = new System.Drawing.Size(244, 127);
             this.button4.TabIndex = 4;
             this.button4.Text = "Student payment information";
             this.button4.UseVisualStyleBackColor = false;
@@ -149,18 +155,20 @@
             this.flowLayoutPanel1.Controls.Add(this.button9);
             this.flowLayoutPanel1.Controls.Add(this.button10);
             this.flowLayoutPanel1.Controls.Add(this.panel2);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 499);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 768);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(163, 38);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(244, 58);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
             // button9
             // 
             this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
             this.button9.Font = new System.Drawing.Font("Copperplate Gothic Bold", 8.25F);
-            this.button9.Location = new System.Drawing.Point(3, 3);
+            this.button9.Location = new System.Drawing.Point(4, 5);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(86, 35);
+            this.button9.Size = new System.Drawing.Size(129, 54);
             this.button9.TabIndex = 7;
             this.button9.Text = "Back to System Menu";
             this.button9.UseVisualStyleBackColor = false;
@@ -169,28 +177,29 @@
             // 
             this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
             this.button10.Font = new System.Drawing.Font("Copperplate Gothic Bold", 6F);
-            this.button10.Location = new System.Drawing.Point(95, 3);
+            this.button10.Location = new System.Drawing.Point(141, 5);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(58, 35);
+            this.button10.Size = new System.Drawing.Size(87, 54);
             this.button10.TabIndex = 8;
             this.button10.Text = "Save changes";
             this.button10.UseVisualStyleBackColor = false;
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(2, 43);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(3, 67);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(133, 65);
+            this.panel2.Size = new System.Drawing.Size(200, 100);
             this.panel2.TabIndex = 9;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(163)))), ((int)(((byte)(38)))));
             this.button3.Font = new System.Drawing.Font("Copperplate Gothic Bold", 15F);
-            this.button3.Location = new System.Drawing.Point(3, 321);
+            this.button3.Location = new System.Drawing.Point(4, 494);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(163, 83);
+            this.button3.Size = new System.Drawing.Size(244, 127);
             this.button3.TabIndex = 3;
             this.button3.Text = "Student monitoring sheet";
             this.button3.UseVisualStyleBackColor = false;
@@ -198,21 +207,23 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::student_management_system.Properties.Resources.logo1;
-            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Location = new System.Drawing.Point(4, 5);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(163, 134);
+            this.pictureBox2.Size = new System.Drawing.Size(244, 205);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
             // StudentManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1002, 549);
+            this.ClientSize = new System.Drawing.Size(1503, 845);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "StudentManagement";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.StudentManagement_Load);
